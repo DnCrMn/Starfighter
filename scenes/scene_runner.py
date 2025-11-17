666ba38e2fa23577385a56604ba1f2208d3738ea
DnCrMn <dancarrsonmendoza@gmail.com>
2025-11-17 12:52:41 -0600

    Balance tweaks to enemies, items, and ships

--- scenes/scene_runner.py
+++ scenes/scene_runner.py
@@ -4,15 +4,15 @@ import json
 from systems.encounters import triggerRandomEncounter
 from utilities.utils import getIntChoice, slowPrint, printASCII
 
-MIN_ENCOUNTERS_BEFORE_BOSS = 5 # Minimum encounters before you get to the boss.
-MAX_ENCOUNTERS_BEFORE_BOSS = 10 # Maximum encounters before you get to the boss.
+MIN_ENCOUNTERS_BEFORE_BOSS = 10 # Minimum encounters before you get to the boss.
+MAX_ENCOUNTERS_BEFORE_BOSS = 15 # Maximum encounters before you get to the boss.
 
 def runPath(player, pathsFile="data/paths.json"):
     with open(pathsFile) as f:
         paths = json.load(f)
 
     current = "prologue"
-    enemyEncountersDone = 0 # Encounters done. Only counts enemy encounters
+    encountersDone = 0 # Encounters done. Could be both item or combat encounter.
 
     while True:
         scene = paths[current] # Get the current path of the player
@@ -26,14 +26,14 @@ def runPath(player, pathsFile="data/paths.json"):
 
         if scene.get("encounter_type") == "boss":
             # Only encounter boss if you have reached the minimum amount of encounters
-            if enemyEncountersDone < MIN_ENCOUNTERS_BEFORE_BOSS:
+            if encountersDone < MIN_ENCOUNTERS_BEFORE_BOSS:
                 slowPrint("You sense something VERY DANGEROUS ahead. You feel unprepared, so you go to a different path", 0.03)
 
                 # Grab any scene that is not a boss type
                 detour = [key for key in paths if key != "boss_gate"]
                 current = random.choice(detour) 
 
-                continue
+                continue # Skip to the next loop and go to a random path
 
             print()
             # If descriptions is a list, print each line
@@ -46,9 +46,9 @@ def runPath(player, pathsFile="data/paths.json"):
             survived = triggerRandomEncounter(player, final = True) 
 
             if survived:
-                slowPrint("YOU WIN! The boss has been defeated!")
-            else:
-                break
+                slowPrint("YOU WIN! The boss has been defeated! The evil that was terrorizing the universe is no more!")
+
+            break # Stop looping after the boss whether you win or not
         
         print()
         # If descriptions is a list, print each line
@@ -58,10 +58,12 @@ def runPath(player, pathsFile="data/paths.json"):
         else:
             slowPrint(desc)
 
+        print(f"\nEnemy Encounters Done: {encountersDone}")
+
         # --- Encounter handling ---
         if scene.get("encounter_type") == "random":
             survived = triggerRandomEncounter(player) 
-            enemyEncountersDone += 1
+            encountersDone += 1
 
             # Game over 
             if not survived:
@@ -87,7 +89,7 @@ def runPath(player, pathsFile="data/paths.json"):
             nextOptions = scene["next"]
 
             # If boss is unlocked and the next path leads to boss, force it when you exceed the max encounters
-            if enemyEncountersDone >= MAX_ENCOUNTERS_BEFORE_BOSS:
+            if encountersDone >= MAX_ENCOUNTERS_BEFORE_BOSS:
                 bossNext = [option for option in nextOptions if paths[option].get("encounter_type") == "boss"]
 
                 # If boss next is not empty
